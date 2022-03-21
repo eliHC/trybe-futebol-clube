14e32af6462f042c28619bd328ba73fd050891d7
eliHC <leonardohcds@gmail.com>
2022-03-21 20:05:24 -0300

    i dont even know anymore

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,8 +1,9 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as bcryptJS from 'bcryptjs';
+
 import chaiHttp = require('chai-http');
 
-import { Response } from 'superagent';
 import { app } from '../app';
 
 import User from '../database/models/User';
@@ -13,8 +14,6 @@ const { expect } = chai;
 
 describe('test app', () => {
   describe('About route /login...', () => {
-    let response: Response;//<< WHY?!!?
-
     const inputPayload = {
       email: "admin@admin.com",
       password: "secret_admin"
@@ -30,22 +29,25 @@ describe('test app', () => {
       token: 'eyJhbGciOiJIUzI1NiJ9.MQ.dQmeXYvGEvAIr4s20zDCeYcI0HxMZhp26RK-4zvGrhQ'
     };
 
-    before(async () => {
+    before(() => {
       
       sinon.stub(User, 'findOne').resolves(outputPayload.user as User);
+      sinon.stub(bcryptJS, 'compareSync').returns(true);
     });
     
-    after(() => {(User.findOne as sinon.SinonStub).restore()});
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+      (bcryptJS.compareSync as sinon.SinonStub).restore();
+    });
+    
     
     it('When the login occurs correctly:', async () => {
-      response = await chai
+      const response = await chai
         .request(app)
         .post('/login')
         .send(inputPayload);
-      // expect(response.body.success).to.be.equal(true);
-      // expect(response.body.message).to.be.equal('OK');
+
       expect(response.status).to.be.equal(200);
-      // expect(response.body).to.deep.equal(outputPayload);
     });
   });
 });
